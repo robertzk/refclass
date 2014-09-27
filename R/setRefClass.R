0b87eede67370b01910489f0834854160e38623c
RobertZK <technoguyrob@gmail.com>
2014-09-26 21:52:11 -0500

    initial function definition for setRefClass

--- R/setRefClass.R
+++ R/setRefClass.R
@@ -14,20 +14,24 @@
 #'   In this particular case, the convention is something like
 #'   \code{list(id = "numeric", data = "data.frame")} to specify two fields,
 #'   \code{id} and \code{data} with required types \code{numeric} and 
-#'   \code{data.frame}.
+#'   \code{data.frame}. The default is \code{character()}.
 #' @param contains character. The parent reference classes of this reference
-#'   class. # TODO: (RK) Document this behavior better.
+#'   class. The default is \code{character()}. # TODO: (RK) Document this behavior better.
 #' @param methods list. The methods for instances of this reference class.
 #'   In particular, the names of this list represent the names of the methods,
 #'   and the values must be functions. For example,
 #'    \code{list(foo = function(x) print(x)} would define a method \code{foo}
 #'   that we could call with \code{reference_class_object$foo(...)}.
+#'   The default is to default is to define no methods (\code{list()}).
 #' @param where environment. Where to store the definition of the reference
-#'   class.
+#'   class. The default is \code{topenv(parent.frame())}.
 #' @param ... # TODO: (RK) Figure out what this does!
 #' @return the fully formed reference class object generator. For example,
 #'   if we call \code{x <- setRefClass(...)}, then we can call \code{x$new(...)}
 #'   to invoke the constructor for the object \code{x}. The class of the
 #'   reference class object generator is \code{"refObjectGenerator"}.
 #' # TODO: (RK) More documentation and examples.
-NULL
+setRefClass <- function(Class, fields = character(), contains = character(),
+                        methods = list(), where = topenv(parent.frame()), ...) {
+  NULL
+}
