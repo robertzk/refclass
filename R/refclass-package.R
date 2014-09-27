fd25307aa2785f83ec83764134a8bc5cfb04a5b2
RobertZK <technoguyrob@gmail.com>
2014-09-27 00:31:52 -0500

    mv -package.R file

---
+++ R/refclass-package.R
@@ -0,0 +1,8 @@
+#' R reference classes.
+#'
+#' This package is an attempt to refactor R's built-in reference class
+#' code to make it more understandable, documented, and tested.
+#'
+#' @name refclass
+#' @docType package
+NULL
