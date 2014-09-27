a0580d8f9c62134b90e818d106a587adf7692762
RobertZK <technoguyrob@gmail.com>
2014-09-27 01:07:48 -0500

    final tests for get_superclasses_information

--- inst/tests/test-get_superclasses_information.R
+++ inst/tests/test-get_superclasses_information.R
@@ -5,3 +5,19 @@ test_that('it returns the trivial stuff on an empty list', {
                    list(superClasses = character(0), isRefSuperClass = logical(0)))
 })
 
+test_that('it gives an error when the contains argument is not convertible to classes', {
+  expect_error(get_superclasses_information(5), 'should be the names of superclasses')
+})
+
+test_that('it errors when super class names are missing definitions', {
+  expect_error(get_superclasses_information('blah', environment()), 'is not a defined class')
+})
+
+test_that('it can get the superclass information for a simple example', {
+  setClass('test__a')
+  methods::setRefClass('test__b')
+  expect_identical(get_superclasses_information(c('test__a', 'test__b'), environment()),
+                   list(superClasses = c('test__a', 'test__b'), isRefSuperClass = c(FALSE, TRUE)))
+})
+
+
