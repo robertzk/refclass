35b33a99e5837718a937a0215ae09c4870111ef1
RobertZK <technoguyrob@gmail.com>
2014-09-26 21:54:47 -0500

    Initial documentation for inferProperties

---
+++ R/inferProperties.R
@@ -0,0 +1,10 @@
+#' Infer the properties of the fields of a reference class.
+#'
+#' # TODO: (RK) Figure out what inferring means...
+#'
+#' @param props 
+#' @param what
+inferProperties <- function(props, what) {
+  NULL
+}
+
