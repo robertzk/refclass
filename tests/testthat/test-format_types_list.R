09bdc68bec0d7f77cf1a7b4191e62cbc74d18de9
RobertZK <technoguyrob@gmail.com>
2014-09-27 00:02:17 -0500

    initial tests for format_types_list

---
+++ tests/testthat/test-format_types_list.R
@@ -0,0 +1,21 @@
+context('format_types_list')
+
+#' stopifnot(identical(format_types_list(c(a = 'character', b = 'data.frame')),
+#'   list(a = 'character', b = 'data.frame')))
+#' stopifnot(identical(format_types_list(c('a', 'b')), list(a = 'ANY', b = 'ANY')))
+#' stopifnot(identical(format_types_list(list(a = 'character')), list(a = 'character')))
+#' # The following will error because of duplicate names:
+
+test_that('it correctly formats a simple character example', {
+  expect_identical(format_types_list(c(a = 'character', b = 'data.frame')),
+    list(a = 'character', b = 'data.frame'))
+})
+
+test_that('it correctly formats an unnamed character example', {
+  expect_identical(format_types_list(c('a', 'b')), list(a = 'ANY', b = 'ANY'))
+})
+
+test_that('it correctly formats a list example', {
+  expect_identical(format_types_list(list(a = 'character')), list(a = 'character'))
+})
+
