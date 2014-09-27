17174319af0ab647138e1fff0b024154caf23cf6
RobertZK <technoguyrob@gmail.com>
2014-09-26 21:43:42 -0500

    Initial commenting for setRefClass

---
+++ R/setRefClass.R
@@ -0,0 +1,10 @@
+#' Create a new reference class.
+#'
+#' Reference class objects are the closest approximation of the concepts of
+#' object-oriented programming available in R. While S3 and S4 classes serve
+#' the useful purpose of providing dispatching mechanisms on generic functions,
+#' reference classes allow for mutability of data on the underlying object
+#' regardless of calling convention (or nasty syntax like replace methods).
+#'
+#' # TODO: (RK) More description here.
+NULL
