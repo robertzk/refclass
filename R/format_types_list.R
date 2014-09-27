c6c48e94557fb81c36bafe8395875dfcbc887a8a
RobertZK <technoguyrob@gmail.com>
2014-09-26 23:59:05 -0500

    Document format_types_list

--- R/format_types_list.R
+++ R/format_types_list.R
@@ -6,16 +6,47 @@
 #' ensure that format is correct, make small modifications if necessary,
 #' and error if there are problems.
 #'
-#' The following are valid formats:
+#' The following are valid formats for \code{props}:
 #'
-#' \items{
+#' \itemize{
+#'   \item{"character"}{"A named character vector with non-empty and distinct
+#'     names.}
+#'   \item{"list"}{"A named list with non-empty and distinct names, and
+#'      character values of length 1."}
+#'   \item{"unnamed character"}{"A totally unnamed character vector with
+#'      non-empty and distinct values. These will be turned into the names
+#'      of a character vector with values \code{'ANY'}.
+#'   }
 #' }
+#' 
+#' If the provided \code{props} does not satisfy any of these, an error
+#' will be invoked.
 #'
 #' # TODO: (RK) Figure out what inferring means...
 #'
-#' @param props ANY
-#' @param what ANY
-format_types_list <- function(props, what) {
+#' @param props character or list. See full description of this function.
+#' @param what character. The name of the thing we are creating a
+#'   types list for. The default is \code{"fields"}. This will be used
+#'   to give more descriptive error messages.
+#' @return a list that is guaranteed to have distinct non-zero length
+#'   names and values that are of type character of length 1.
+#' @examples
+#' stopifnot(identical(format_types_list(c(a = 'character', b = 'data.frame')),
+#'   list(a = 'character', b = 'data.frame')))
+#' stopifnot(identical(format_types_list(c('a', 'b')), list(a = 'ANY', b = 'ANY')))
+#' stopifnot(identical(format_types_list(list(a = 'character')), list(a = 'character')))
+#' # The following will error because of duplicate names:
+#' format_types_list(list(a = 'character', b = 'character'))
+format_types_list <- function(props, what = "fields") {
+  # If an unnamed vector of characters, assume these are the names
+  # and the types are all ANY.
+  if (is.character(props) && is.null(names(props)))
+    props <- setNames(rep('ANY', length(props)), props)
 
-  NULL
+  if (!is.character(props) && !is.list(props))
+    stop(gettextf("argument %s must be a list or a character vector; got an object of class %s",
+                  dQuote(what), dQuote(class(fields)[1])), domain = NA, call. = FALSE)
+
+  names_are_unique_and_non_empty(props, what, error = TRUE)
+  as.list(props)
 }
