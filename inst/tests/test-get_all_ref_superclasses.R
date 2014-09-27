603a743ae8d7dfdc9afd6d246c2e9f8c8adeb988
RobertZK <technoguyrob@gmail.com>
2014-09-27 13:28:51 -0500

    remove duplicate test

--- inst/tests/test-get_all_ref_superclasses.R
+++ inst/tests/test-get_all_ref_superclasses.R
@@ -1,3 +1,11 @@
 context('get_all_ref_superclasses')
 
+test_that('it can get all reference superclasses for a simple example', {
+  methods::setRefClass('test__c')
+  methods::setRefClass('test__d', contains = 'test__c')
+  expect_equal(get_all_ref_superclasses(list(getClass('test__d'))),
+               c('test__c', 'envRefClass'))
+  removeClass('test__c')
+  removeClass('test__d')
+})
 
