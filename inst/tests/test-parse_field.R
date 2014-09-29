dcb73b42be6cf9a767c9d3ea20d5a197b4739c3a
RobertZK <technoguyrob@gmail.com>
2014-09-29 11:38:17 -0500

    active binding field value parsing test pending

--- inst/tests/test-parse_field.R
+++ inst/tests/test-parse_field.R
@@ -1,4 +1,5 @@
 context('parse_field')
+library(testthatsomemore)
 
 test_that('it errors when a non-1 length field value gets passed', {
   expect_error(parse_field('foo', c('a','b')), "single class name is needed")
@@ -11,3 +12,8 @@ test_that('it errors when a non-existent class gets passed', {
 test_that('it creates an unitializedField prototype for ANY field classes', {
   expect_equal(as.character(class(parse_field('foo', 'ANY')[[2]])), 'uninitializedField') 
 })
+
+test_that('it creates an activeBindingFunction when the field value is a function', {
+  pending()
+})
+
